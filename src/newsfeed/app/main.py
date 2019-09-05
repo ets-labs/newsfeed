21424a9e3b6b83d13fecb7bb8eb1a1fc57d469a8
Roman Mogylatov <rmogilatov@gmail.com>
2019-09-05 13:03:38 +0300

    Run event publisher processing in background

--- src/newsfeed/app/main.py
+++ src/newsfeed/app/main.py
@@ -1,5 +1,6 @@
 """Main module."""
 
+import asyncio
 import os
 
 from aiohttp import web
@@ -7,10 +8,34 @@ from aiohttp import web
 from .application import Application
 
 
+async def event_publishing(web_app):
+    """Publish events."""
+    event_publisher_service = web_app.domain_model.event_publisher_service()
+    while True:
+        await event_publisher_service.process_event()
+
+
+async def start_background_tasks(web_app: web.Application):
+    """Start application background tasks."""
+    loop = asyncio.get_event_loop()
+    web_app['event_publishing'] = loop.create_task(event_publishing(web_app))
+
+
+async def cleanup_background_tasks(app):
+    """Stop application background tasks."""
+    app['event_publishing'].cancel()
+    await app['event_publishing']
+
+
 def main():
     """Run application."""
     app = Application()
+
     web_app = app.create_web_app()
+
+    web_app.on_startup.append(start_background_tasks)
+    web_app.on_cleanup.append(cleanup_background_tasks)
+
     web.run_app(web_app, port=int(os.getenv('PORT')))
 
 
