8196f981f9d35cdfb561d5066ac504648476a5b6
Roman Mogylatov <rmogilatov@gmail.com>
2019-09-23 16:52:05 -0400

    Add docs for GET /events

--- src/newsfeed/packages/webapi/handlers/misc.py
+++ src/newsfeed/packages/webapi/handlers/misc.py
@@ -25,6 +25,147 @@ OPENAPI_SCHEMA = {
     },
     'servers': [],
     'paths': {
+        '/newsfeed/{newsfeed_id}/events/': {
+            'get': {
+                'summary': 'Return newsfeed events',
+                'operationId': 'get_newsfeed_events',
+                'tags': [
+                    'Events',
+                ],
+                'parameters': [
+                    {
+                        'in': 'path',
+                        'name': 'newsfeed_id',
+                        'required': True,
+                        'schema': {
+                            'type': 'integer',
+                        },
+                    },
+                ],
+                'responses': {
+                    '200': {
+                        'description': 'List of newsfeed events',
+                        'content': {
+                            'application/json': {
+                                'schema': {
+                                    '$ref': '#/components/schemas/NewsfeedEventsList',
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+
+        # '/sign-offs/{signoff_id}/': {
+        #     'get': {
+        #         'summary': 'Return specified sign-off details',
+        #         'operationId': 'get_signoff',
+        #         'tags': [
+        #             'Sign-offs',
+        #         ],
+        #         'parameters': [
+        #             {
+        #                 'in': 'path',
+        #                 'name': 'signoff_id',
+        #                 'required': True,
+        #                 'schema': {
+        #                     'type': 'integer',
+        #                 },
+        #             },
+        #         ],
+        #         'responses': {
+        #             '200': {
+        #                 'description': 'Sign-off details',
+        #                 'content': {
+        #                     'application/json': {
+        #                         'schema': {
+        #                             '$ref': '#/components/schemas/Signoff',
+        #                         },
+        #                     },
+        #                 },
+        #             },
+        #         },
+        #     },
+        #     'put': {
+        #         'summary': 'Update specified sign-off',
+        #         'operationId': 'update_signoff',
+        #         'tags': [
+        #             'Sign-offs',
+        #         ],
+        #         'parameters': [
+        #             {
+        #                 'in': 'path',
+        #                 'name': 'signoff_id',
+        #                 'required': True,
+        #                 'schema': {
+        #                     'type': 'integer',
+        #                 },
+        #             },
+        #         ],
+        #         'requestBody': {
+        #             'required': True,
+        #             'content': {
+        #                 'application/json': {
+        #                     'schema': {
+        #                         'properties': {
+        #                             'name': {
+        #                                 'type': 'string',
+        #                                 'example': 'golang-developer-roadmap',
+        #                             },
+        #                             'contribution_type': {
+        #                                 'type': 'string',
+        #                                 'example': 'medium',
+        #                             },
+        #                             'repository': {
+        #                                 'type': 'integer',
+        #                                 'example': 123,
+        #                             },
+        #                             'approver': {
+        #                                 'type': 'string',
+        #                                 'example': 'agreen',
+        #                             },
+        #                         },
+        #                     },
+        #                 },
+        #             },
+        #         },
+        #         'responses': {
+        #             '200': {
+        #                 'description': 'Sign-off has been successfully updated',
+        #                 'content': {
+        #                     'application/json': {
+        #                         'schema': {
+        #                             '$ref': '#/components/schemas/Signoff',
+        #                         },
+        #                     },
+        #                 },
+        #             },
+        #         },
+        #     },
+        #     'delete': {
+        #         'summary': 'Delete specified sign-off',
+        #         'operationId': 'delete_signoff',
+        #         'tags': [
+        #             'Sign-offs',
+        #         ],
+        #         'parameters': [
+        #             {
+        #                 'in': 'path',
+        #                 'name': 'signoff_id',
+        #                 'required': True,
+        #                 'schema': {
+        #                     'type': 'integer',
+        #                 },
+        #             },
+        #         ],
+        #         'responses': {
+        #             '204': {
+        #                 'description': 'Sign-off has been successfully deleted',
+        #             },
+        #         },
+        #     },
+        # },
         '/status/': {
             'get': {
                 'summary': 'Return current microservice status',
@@ -56,6 +197,24 @@ OPENAPI_SCHEMA = {
     },
     'components': {
         'schemas': {
+            'NewsfeedEvent': {
+                'properties': {
+                    'id': {
+                        'type': 'string',
+                        'format': 'uuid',
+                    },
+                },
+            },
+            'NewsfeedEventsList': {
+                'properties': {
+                    'results': {
+                        'type': 'array',
+                        'items': {
+                            '$ref': '#/components/schemas/NewsfeedEvent',
+                        },
+                    },
+                },
+            },
         },
     },
 }
