ac51f7dd136829d9abc82f0fc257f22b48aed7be
Roman Mogylatov <rmogilatov@gmail.com>
2019-09-30 17:27:53 -0400

    Make some cosmetic changes with typing

--- src/newsfeed/app/factory.py
+++ src/newsfeed/app/factory.py
@@ -1,14 +1,12 @@
 """Factory module."""
 
-from typing import Callable
-
 from dependency_injector import providers
 
 from .application import Application
 from .containers import Infrastructure, DomainModel, WebApi
 
 
-application_factory: Callable[..., Application] = providers.Factory(
+application_factory = providers.Factory(
     Application,
     infrastructure=providers.Factory(
         Infrastructure,
