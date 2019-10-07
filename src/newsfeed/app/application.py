122d644c479ac5010423628523becfe07cbcff68
Roman Mogylatov <rmogilatov@gmail.com>
2019-10-07 09:26:39 -0400

    Move main() to Application (#36)

--- src/newsfeed/app/application.py
+++ src/newsfeed/app/application.py
@@ -1,5 +1,10 @@
 """Application module."""
 
+import asyncio
+import os
+
+from aiohttp import web
+
 from .containers import Infrastructure, DomainModel, WebApi
 
 
@@ -15,3 +20,26 @@ class Application:
 
         self.web_api = web_api
         self.web_api.domain.override(self.domain_model)
+
+    def main(self):
+        """Run application."""
+        web_app: web.Application = self.web_api.web_app()
+
+        web_app.on_startup.append(self._start_background_tasks)
+        web_app.on_cleanup.append(self._cleanup_background_tasks)
+
+        web.run_app(web_app, port=int(os.getenv('PORT')))
+
+    async def _start_background_tasks(self, web_app: web.Application):
+        loop = asyncio.get_event_loop()
+        web_app['event_publishing'] = loop.create_task(self._event_publishing_task())
+
+    async def _cleanup_background_tasks(self, web_app: web.Application):
+        web_app['event_publishing'].cancel()
+        await web_app['event_publishing']
+
+    async def _event_publishing_task(self):
+        """Publish events."""
+        event_publisher_service = self.domain_model.event_publisher_service()
+        while True:
+            await event_publisher_service.process_event()
