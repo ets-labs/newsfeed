264b3b3b7e23e40006e336fcce2fa2a66270c5e6
Roman Mogylatov <rmogilatov@gmail.com>
2019-10-15 21:27:28 -0400

    Update base path

--- scripts/integration_check.py
+++ scripts/integration_check.py
@@ -265,4 +265,4 @@ async def main(url):
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(url='http://127.0.0.1:8000/'))
+    loop.run_until_complete(main(url='http://127.0.0.1:8000/api/'))
