a961cba26158490f527696ddedee6b15cc18934c
Roman Mogylatov <rmogilatov@gmail.com>
2019-10-16 08:49:32 -0400

    Rename AsyncInMemoryEventQueue -> InMemoryEventQueue (#58)

--- src/newsfeed/app/containers.py
+++ src/newsfeed/app/containers.py
@@ -11,7 +11,7 @@ class Infrastructure(containers.DeclarativeContainer):
     config = providers.Configuration('infrastructure')
 
     event_queue = providers.Singleton(
-        infrastructure.event_queues.AsyncInMemoryEventQueue,
+        infrastructure.event_queues.InMemoryEventQueue,
         config=config.event_queue,
     )
 
