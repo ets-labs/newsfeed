43d1a74a48b8e37f1bf155be4bec4ee8b00d1920
mrbish <mrbish321@gmail.com>
2019-10-21 13:12:47 +0300

    Add build folder to the repo

---
+++ demo_app/build/precache-manifest.b090df55e00e3aca1de47f1c01d8791e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6f90ad9f2dc0ffa3c1ab0e976306c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2788b19544f1f201effa",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "d654c9c497fe18cbb4f8",
+    "url": "/static/css/main.959f4292.chunk.css"
+  },
+  {
+    "revision": "2788b19544f1f201effa",
+    "url": "/static/js/2.c2b9ed56.chunk.js"
+  },
+  {
+    "revision": "d654c9c497fe18cbb4f8",
+    "url": "/static/js/main.6a19aded.chunk.js"
+  },
+  {
+    "revision": "8dd5169fc8845bf2f6ff",
+    "url": "/static/js/runtime-main.b9522ce4.js"
+  }
+]);
